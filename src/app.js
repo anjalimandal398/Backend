916fabf342615f082ca7bc679a58d5247f27bc1c
anjalimandal398 <anjalikumarimandaljms@gmail.com>
2025-07-29 12:42:06 +0530

    exploring get,post,delete methods

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
-const express=require("express")
-const app=express();
+const express = require("express");
+const app = express();
 
-app.use('/home',(req,res)=>{
-    res.send("This is Home Page");
-    
-})
-app.use('/contact',(req,res)=>{
-    res.send("This is Contact Page");
-    
-})
 
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
-    
-})
+//This will only handle the GET call
+app.get("/user", (req, res) => {
+  res.send({ FirstName: "Anjali", LastName: "Kumari" });
+});
+
+
+//This will only handle the post call
+app.post("/user", (req, res) => {
+  res.send( "This is user page by POST method");
+});
+
+
+//This will only handle the delete call
+app.delete("/user", (req, res) => {
+    res.send("Deleted successfully");
+  });
+
+
+//it will match all the HTTP methods
+app.use("/test", (req, res) => {
+  res.send("This is test  Page");
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
