080fdef670313c037595b83ee5425412d2e6c4ef
anjalimandal398 <anjalikumarimandaljms@gmail.com>
2025-08-15 16:32:10 +0530

    Authentication

--- src/app.js
+++ src/app.js
@@ -43,14 +43,11 @@ app.post("/login", async (req, res) => {
     if (!user) {
       throw new Error("Invalid email or password");
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await user.validatePassword (password);
     if (isPasswordValid) {
-      //Create a JWT Token
-      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder$790",{
-        expiresIn:"15d",
-      });
 
-      //Add the token to Cookie and send the response back to user
+      const token = await user.getJWT();
+ 
       res.cookie("token", token);
 
       res.send("Login Successful!!");
--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,7 @@
 const mongoose=require("mongoose");
+const jwt = require("jsonwebtoken");
 const validator=require("validator");
+const bcrypt = require("bcrypt"); 
 
 
 const userSchema=new mongoose.Schema({
@@ -68,6 +70,27 @@ const userSchema=new mongoose.Schema({
    timestamps:true, 
 }
 )
+userSchema.methods.getJWT= async function(){
+    const user = this;
+
+   const token =await jwt.sign({ _id: user._id }, "DEV@Tinder$790",{
+        expiresIn:"15d",
+      });
+      return token;
+}
+
+userSchema.methods.validatePassword=async function(passwordInputByUser){
+    const user=this;
+    const passwordHash=user.password
+   const isPasswordValid=await bcrypt.compare(
+    passwordInputByUser,
+     passwordHash
+    );
+    return isPasswordValid;
+
+}
+
+
 
 const UserModel=mongoose.model("User",userSchema)
 
